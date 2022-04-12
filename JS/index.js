2c5903dd50d348390f66a9034c6bf2cb2b3b8d98
poltergeist143 <poltergeistaylor@gmail.com>
2022-04-12 16:53:19 +0800

    change image

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,9 @@
 
 function myFunction() {
         document.getElementById("demo").innerHTML = "Paragraph changed.";
+        document.getElementById("image").src="./images/sadme.jpg "
+        document.getElementById("image").style.height = "250px"   
+        document.getElementById("image").style.weight = "250px"   
   }
+
 
