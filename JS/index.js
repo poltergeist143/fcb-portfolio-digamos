2fc5926c27401767a76d136634d44006c92e7eda
poltergeist143 <poltergeistaylor@gmail.com>
2022-04-12 19:46:32 +0800

    change text

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,6 @@
-
 function myFunction() {
-        document.getElementById("demo").innerHTML = "Paragraph changed.";
+        alert("Changes Saved!")
+        document.getElementById("intro").innerHTML = "Ga maoy pako! Hahahaha";
         document.getElementById("image").src="./images/sadme.jpg "
         document.getElementById("image").style.height = "250px"   
         document.getElementById("image").style.weight = "250px"   
